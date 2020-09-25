2fdb92e98c9853929c43043bf525a71acb0c64af
Jiafeng Qiu <994062163@qq.com>
2020-09-25 21:26:29 +0800

    Swirl lesson

---
+++ Exploratory Data Analysis.R
@@ -0,0 +1,9 @@
+#Lesson 1 Principles of Analytic Graphs
+##Principle 1: Comparison.
+##Principle 2: Show causality or a mechanism of how your theory of the data works.
+##Principle 3: Multivariate（多元） data.
+##Principle 4: Integrating evidence.
+##Principle 5: Describe and document the evidence with sources and appropriate labels and scales.
+##Principle 6: Content is King!
+
+#Lesson 2 
