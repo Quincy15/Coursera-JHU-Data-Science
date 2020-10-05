3b66e99c86c1f26498814a98eb50574851a8fe59
Jiafeng Qiu <994062163@qq.com>
2020-10-05 14:49:55 +0800

    Updating Lesson 5

--- Exploratory Data Analysis.R
+++ Exploratory Data Analysis.R
@@ -74,4 +74,34 @@ head(state); table(state$region)#看变量有多少个值
 xyplot(Life.Exp ~ Income | region, data = state, layout = c(4,1))
 #3. ggplot2
 qplot(displ, hwy, data = mpg)
-#Lesson 5 
+
+#Lesson 5 Base Plotting System
+head(airquality)
+range(airquality$Ozone, na.rm = TRUE)#returns a vector containing the minimum and maximum of all the given arguments.
+hist(airquality$Ozone)
+table(airquality$Month)
+boxplot(Ozone~Month, airquality)
+boxplot(Ozone~Month, airquality, xlab = "Month", ylab = "Ozone(ppb)", col.axis = "blue", col.lab = "red", main = "Ozone and Wind in New York City")#specifying some arguments
+with(airquality, plot(Wind, Ozone))
+title(main="Ozone and Wind in New York City")
+length(par())
+#The par() function is used to specify global graphics parameters that affect all plots in an R session.
+par()$pin;par("fg")
+plot(airquality$Wind, airquality$Ozone,type = "n")
+title(main = "Wind and Ozone in NYC")
+may <- subset(airquality, Month == 5)
+points(may$Wind,may$Ozone,col="blue",pch=17)
+notmay <- subset(airquality, Month != 5)
+points(notmay$Wind,notmay$Ozone,col="red",pch=8)
+legend("topright", pch = c(17,8), col = c("blue", "red"), legend = c("May", "Other Months"))#图例
+abline(v=median(airquality$Wind),lty=2,lwd=2)
+par(mfrow = c(1,2))
+plot(airquality$Wind, airquality$Ozone, main = "Ozone and Wind")
+plot(airquality$Ozone, airquality$Solar.R, main = "Ozone and Solar Radiation")
+par(mfrow = c(1,3), mar = c(4,4,2,1), oma = c(0,0,2,0))
+plot(airquality$Wind, airquality$Ozone, main = "Ozone and Wind")
+plot(airquality$Solar.R, airquality$Ozone, main = "Ozone and Solar Radiation")
+plot(airquality$Temp, airquality$Ozone, main = "Ozone and Temperature")
+mtext("Ozone and Weather in New York City", outer = TRUE)
+
+Lesson 6
